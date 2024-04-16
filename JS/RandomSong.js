637a627281ea1995391f953e73f01c93a21f9f75
Obama <153317984+ObamaTMyt@users.noreply.github.com>
2024-04-16 19:05:05 +0200

    fix

--- JS/RandomSong.js
+++ JS/RandomSong.js
@@ -4,17 +4,11 @@ window.onload = function () {
         "Songs/Stockholm.mp4",
         "Songs/Maladin.mp4",
         "Songs/Toto.mp4",
-        "Songs/Zombie.mp4",
         "Songs/Air.mp4",
         "Songs/Catwalk.mp4",
         "Songs/Himmlen.mp4",
-        "Songs/Områden.mp4",
         "Songs/Vi.mp4",
-        "Songs/Reveng.mp4",
-        "Songs/Russian.mp4",
         "Songs/Stand.mp4",
-        "Songs/Trakter.mp4",
-        "Songs/Yamaksi.mp4",
         "Songs/Decible.mp4",
     ];
     
