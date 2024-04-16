cf188c15f76d5f623da0375f8c50878ca686b6e9
Obama <153317984+ObamaTMyt@users.noreply.github.com>
2024-04-16 22:13:08 +0200

    dying

--- JS/PlayOnClick.js
+++ JS/PlayOnClick.js
@@ -1,3 +1,5 @@
+var videoStarted = false; 
+
 function toggleVideo() {
     var startScreen = document.getElementById("startScreen");
     startScreen.style.display = "none";
@@ -6,8 +8,11 @@ function toggleVideo() {
     var video = document.getElementById("videos");
     videoContainer.style.display = "block";
 
-    if (video.paused) {
-        video.play();
+    if (!videoStarted) {
+        if (video.paused) {
+            video.play();
+            videoStarted = true; 
+        }
     }
 
     var profile = document.querySelector(".profile");
@@ -16,3 +21,22 @@ function toggleVideo() {
     profile.style.display = "block";
     socialLinks.style.display = "flex";
 }
+
+window.onload = function () {
+    var songs = [
+        "Songs/göre.mp4",
+        "Songs/Stockholm.mp4",
+        "Songs/Maladin.mp4",
+        "Songs/Toto.mp4",
+        "Songs/Air.mp4",
+        "Songs/Catwalk.mp4",
+        "Songs/Himmlen.mp4",
+        "Songs/Vi.mp4",
+        "Songs/Stand.mp4",
+        "Songs/Decible.mp4",
+    ];
+    
+    var randomIndex = Math.floor(Math.random() * songs.length);
+    var videoElement = document.getElementById('videos');
+    videoElement.src = songs[randomIndex];
+};
